ce8628e5e1ccb9b10a2cd2b1140f4eeac2d5eb46
valfink <117605562+valfink@users.noreply.github.com>
2023-06-22 13:14:14 +0200

    Use WorkingPersonBuilder

--- creational/builder/src/main/java/org/example/Main.java
+++ creational/builder/src/main/java/org/example/Main.java
@@ -2,6 +2,11 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        WorkingPersonBuilder workingPersonBuilder = new WorkingPersonBuilder();
+        workingPersonBuilder
+                .withFirstName("Wolfgang Amadeus")
+                .withLastName("Mozart")
+                .withJob("Composer");
+        System.out.println(workingPersonBuilder.build());
     }
 }
