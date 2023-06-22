434783ff7a61e5f778d0cc7c8030d8c0a28c155c
valfink <117605562+valfink@users.noreply.github.com>
2023-06-22 13:50:32 +0200

    Implement PersonAddressBuilder

--- creational/builder/src/main/java/org/example/facade/PersonBuilder.java
+++ creational/builder/src/main/java/org/example/facade/PersonBuilder.java
@@ -5,6 +5,10 @@ import org.example.Person;
 public class PersonBuilder {
     protected Person person = new Person();
 
+    public PersonAddressBuilder lives() {
+        return new PersonAddressBuilder(person);
+    }
+
     public Person build() {
         return person;
     }
