d2edaa05408a73362fbaa67d08ef7dc5acc721c4
valfink <117605562+valfink@users.noreply.github.com>
2023-06-22 13:37:16 +0200

    Add more fields to Person for faceted builder

--- creational/builder/src/main/java/org/example/Person.java
+++ creational/builder/src/main/java/org/example/Person.java
@@ -7,4 +7,11 @@ public class Person {
     private String firstName;
     private String lastName;
     private String job;
+
+    private String streetAddress;
+    private String postcode;
+    private String city;
+
+    private String companyName;
+    private String annualIncome;
 }
