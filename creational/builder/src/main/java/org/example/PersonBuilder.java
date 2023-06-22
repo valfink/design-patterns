dc8bbc3a540435c1393d62d9b3677b3647f8d003
valfink <117605562+valfink@users.noreply.github.com>
2023-06-22 13:10:23 +0200

    Rename Generic

--- creational/builder/src/main/java/org/example/PersonBuilder.java
+++ creational/builder/src/main/java/org/example/PersonBuilder.java
@@ -1,24 +1,24 @@
 package org.example;
 
-public class PersonBuilder<SELF extends PersonBuilder<SELF>> {
+public class PersonBuilder<S extends PersonBuilder<S>> {
     // can only be used with a Type that extends PersonBuilder!
 
     protected Person person = new Person();
 
-    public SELF withFirstName(String firstName) {
+    public S withFirstName(String firstName) {
         person.setFirstName(firstName);
         return self();
     }
 
-    public SELF withLastName(String lastName) {
+    public S withLastName(String lastName) {
         person.setLastName(lastName);
         return self();
     }
 
-    protected SELF self() {
+    protected S self() {
         // typecasting seems to be the only way here to stay generic
-        // BUT the cast can't fail as SELF is always an extension of PersonBuilder!
-        return (SELF) this;
+        // BUT the cast can't fail as S is always an extension of PersonBuilder!
+        return (S) this;
     }
 
     public Person build() {
