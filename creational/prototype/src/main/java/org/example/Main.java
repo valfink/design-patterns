e1a6dedc9f31a3eeae1a88f3326efd45ea0054fe
valfink <117605562+valfink@users.noreply.github.com>
2023-06-26 09:30:37 +0200

    Use copy constructors on Wolfgang & Ludwig

--- creational/prototype/src/main/java/org/example/Main.java
+++ creational/prototype/src/main/java/org/example/Main.java
@@ -1,7 +1,15 @@
 package org.example;
 
+import org.example.copyconstructor.Address;
+import org.example.copyconstructor.Person;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Person wolfgang = new Person("Wolfgang Amadeus Mozart", new Address("Wiener Str. 1", "12345", "Wien"));
+        Person ludwig = new Person(wolfgang);
+        ludwig.setName("Ludwig van Beethoven");
+        System.out.println("Copy Constructor:");
+        System.out.println(wolfgang);
+        System.out.println(ludwig);
     }
 }
