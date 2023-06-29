077a7386ea8bc3451614b3841e29d80294b87ac4
valfink <117605562+valfink@users.noreply.github.com>
2023-06-29 12:28:54 +0200

    Implement SimpleMultiton

--- creational/singleton/src/main/java/org/example/Main.java
+++ creational/singleton/src/main/java/org/example/Main.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import org.example.multiton.SimpleMultiton;
+import org.example.multiton.Subsystem;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Will get the Basic Singleton now...");
@@ -12,5 +15,19 @@ public class Main {
         System.out.println("Will get the Lazy Singleton now...");
         LazySingleton lazySingleton = LazySingleton.getInstance();
         System.out.println(lazySingleton);
+
+        System.out.println("Will get the first Multiton now...");
+        SimpleMultiton simpleMultiton1 = SimpleMultiton.get(Subsystem.FIRST);
+        System.out.println(simpleMultiton1);
+        System.out.println("Will get the second...");
+        SimpleMultiton simpleMultiton2 = SimpleMultiton.get(Subsystem.SECOND);
+        System.out.println(simpleMultiton2);
+        System.out.println("Now getting another of the second...");
+        SimpleMultiton simpleMultiton1v2 = SimpleMultiton.get(Subsystem.SECOND);
+        System.out.println(simpleMultiton1v2);
+        System.out.println("Now also getting the third...");
+        SimpleMultiton simpleMultiton3 = SimpleMultiton.get(Subsystem.THIRD);
+        System.out.println(simpleMultiton3);
+
     }
 }
