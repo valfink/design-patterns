efff6527019f45c58397b6f8c9c1964a34854c3a
valfink <117605562+valfink@users.noreply.github.com>
2023-06-29 12:28:39 +0200

    Add SimpleMultiton & Subsystem

--- creational/singleton/src/main/java/org/example/multiton/SimpleMultiton.java
+++ creational/singleton/src/main/java/org/example/multiton/SimpleMultiton.java
@@ -1,10 +1,10 @@
 package org.example.multiton;
 
-import java.util.HashMap;
+import java.util.EnumMap;
 import java.util.Map;
 
 public class SimpleMultiton {
-    private static final Map<Subsystem, SimpleMultiton> INSTANCES = new HashMap<>();
+    private static final Map<Subsystem, SimpleMultiton> INSTANCES = new EnumMap<>(Subsystem.class);
 
     private SimpleMultiton() {
         System.out.println("Initializing a new instance. This is instance no. " + (INSTANCES.keySet().size() + 1));
