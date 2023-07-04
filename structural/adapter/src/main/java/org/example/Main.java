61c5071a5ec0918816fffbb44eab29264c072cf3
valfink <117605562+valfink@users.noreply.github.com>
2023-07-04 11:45:58 +0200

    Use StraightLineToPointsBasicAdapter

--- structural/adapter/src/main/java/org/example/Main.java
+++ structural/adapter/src/main/java/org/example/Main.java
@@ -1,7 +1,23 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+    private static void drawPoint(Point point) {
+        System.out.printf("(%2d,%2d): X%n", point.x(), point.y());
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ArrayList<VectorRectangle> vectorRectangles = new ArrayList<>(List.of(
+                new VectorRectangle(1, 1, 10, 2),
+                new VectorRectangle(5, 2, 2, 7)
+        ));
+
+        for (VectorRectangle vectorRectangle : vectorRectangles) {
+            for (Line line : vectorRectangle) {
+                new StraightLineToPointsBasicAdapter(line).forEach(Main::drawPoint);
+            }
+        }
     }
 }
