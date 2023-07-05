5f0c8b5cdf6ccfee6a081c838b15978b4a25c100
valfink <117605562+valfink@users.noreply.github.com>
2023-07-05 16:10:28 +0200

    Use the neurons

--- structural/composite/src/main/java/org/example/Main.java
+++ structural/composite/src/main/java/org/example/Main.java
@@ -2,6 +2,18 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Neuron neuron = new Neuron();
+        Neuron neuron2 = new Neuron();
+        NeuronLayer layer = new NeuronLayer();
+        layer.add(new Neuron());
+        layer.add(new Neuron());
+        NeuronLayer layer2 = new NeuronLayer();
+
+        neuron.connectTo(neuron2);
+        neuron.connectTo(layer);
+        layer.connectTo(neuron);
+        layer.connectTo(layer2);
+
+        neuron.printConnections();
     }
 }
