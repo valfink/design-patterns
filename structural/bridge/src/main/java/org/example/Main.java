bd4457316c8528cf9dc11e44fbe48a997c909c2b
valfink <117605562+valfink@users.noreply.github.com>
2023-07-05 14:27:26 +0200

    Use Bridge

--- structural/bridge/src/main/java/org/example/Main.java
+++ structural/bridge/src/main/java/org/example/Main.java
@@ -2,6 +2,11 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Renderer rasterRenderer = new RasterRenderer();
+        Renderer vectorRenderer = new VectorRenderer();
+        Shape circle = new Circle(rasterRenderer);
+        circle.draw();
+        circle.setRenderer(vectorRenderer);
+        circle.draw();
     }
 }
