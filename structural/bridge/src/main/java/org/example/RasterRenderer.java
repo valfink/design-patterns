4cba4b798abfa003357ba5b962c258524180b9b8
valfink <117605562+valfink@users.noreply.github.com>
2023-07-05 14:16:45 +0200

    Add Renderer, VectorRenderer, RasterRenderer

---
+++ structural/bridge/src/main/java/org/example/RasterRenderer.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class RasterRenderer implements Renderer {
+    @Override
+    public void renderShape() {
+        System.out.println("Drawing shape in pixels :( :)");
+    }
+}
---
+++ structural/bridge/src/main/java/org/example/VectorRenderer.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class VectorRenderer implements Renderer {
+    @Override
+    public void renderShape() {
+        System.out.println("Drawing shape smoothly as V E C T O R");
+    }
+}
