8a891e24bc00deff7f60f457a529f1a9b1616962
valfink <117605562+valfink@users.noreply.github.com>
2023-07-06 11:03:40 +0200

    Use ColoredShape

--- structural/decorator/src/main/java/org/example/Main.java
+++ structural/decorator/src/main/java/org/example/Main.java
@@ -7,5 +7,7 @@ public class Main {
         Shape square = new Square(2.5f);
         System.out.println(circle.info());
         System.out.println(square.info());
+        Shape redCircle = new ColoredShape(new Circle(5), "red");
+        System.out.println(redCircle.info());
     }
 }
