1a2711bb504469fb960e6bb48b314907e28c9015
valfink <117605562+valfink@users.noreply.github.com>
2023-07-06 11:42:26 +0200

    Fix Buffer

--- structural/facade/src/main/java/org/example/Buffer.java
+++ structural/facade/src/main/java/org/example/Buffer.java
@@ -1,12 +1,14 @@
 package org.example;
 
-import lombok.AllArgsConstructor;
-
 // low-level class
-@AllArgsConstructor
 public class Buffer {
-    private char[] chars;
-    private int lineWidth;
+    private final char[] chars;
+    private final int lineWidth;
+
+    public Buffer(int lineWidth, int lineHeight) {
+        this.lineWidth = lineWidth;
+        chars = new char[lineWidth * lineHeight];
+    }
 
     public char charAt(int x, int y) {
         return chars[y * lineWidth + x];
