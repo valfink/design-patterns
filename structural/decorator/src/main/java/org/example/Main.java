8f85b7a932803aa830b99bcc90da8b11f8a61383
valfink <117605562+valfink@users.noreply.github.com>
2023-07-06 11:00:12 +0200

    Use Circle & Square

--- structural/decorator/src/main/java/org/example/Main.java
+++ structural/decorator/src/main/java/org/example/Main.java
@@ -3,6 +3,9 @@ package org.example;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Shape circle = new Circle(3);
+        Shape square = new Square(2.5f);
+        System.out.println(circle.info());
+        System.out.println(square.info());
     }
 }
