a9432e757789b260b32d66b639f7041c125fd31c
valfink <117605562+valfink@users.noreply.github.com>
2023-07-06 13:44:25 +0200

    Add User Flyweight class

---
+++ structural/flyweight/src/main/java/org/example/User.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+// Flyweight class
+public class User {
+    // the externalized (static) storage of actual names
+    private static final List<String> namesList = new ArrayList<>();
+    // the internal storage of references to the names
+    private final int[] namesOfUser;
+
+    // externalize storing the name in constructor
+    public User(String fullName) {
+        namesOfUser = Arrays.stream(fullName.split(" "))
+                .mapToInt(name -> {
+                    int index = namesList.indexOf(name);
+                    if (index != -1) {
+                        return index;
+                    }
+                    namesList.add(name);
+                    return namesList.size() - 1;
+                })
+                .toArray();
+    }
+
+    public String getFullName() {
+        return Arrays.stream(namesOfUser)
+                .mapToObj(namesList::get)
+                .collect(Collectors.joining(" "));
+    }
+}
