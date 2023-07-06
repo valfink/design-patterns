1473f574fcc56b616f031a33b9b5a1f583ffff9a
valfink <117605562+valfink@users.noreply.github.com>
2023-07-06 13:51:23 +0200

    Use User class

--- structural/flyweight/src/main/java/org/example/Main.java
+++ structural/flyweight/src/main/java/org/example/Main.java
@@ -2,6 +2,13 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        User johannSebastianBach = new User("Johann Sebastian Bach");
+        User wolfgangAmadeusMozart = new User("Wolfgang Amadeus Mozart");
+        User sebastianAmadeus = new User("Sebastian Amadeus");
+        System.out.println(johannSebastianBach.getFullName());
+        System.out.println(wolfgangAmadeusMozart.getFullName());
+        System.out.println(sebastianAmadeus.getFullName());
+        System.out.println("All the names in the database:");
+        System.out.println(User.printAllNames());
     }
 }
