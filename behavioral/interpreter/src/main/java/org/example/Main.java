96374441d503761c11d3b8329e375720cb936a64
valfink <117605562+valfink@users.noreply.github.com>
2023-07-11 12:33:58 +0200

    Use the Interpreter

--- behavioral/interpreter/src/main/java/org/example/Main.java
+++ behavioral/interpreter/src/main/java/org/example/Main.java
@@ -1,7 +1,17 @@
 package org.example;
 
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String calculation = "(13-4)-(12+7)";
+        List<Token> tokenList = Interpreter.lex(calculation);
+        System.out.println(tokenList.stream()
+                .map(Objects::toString)
+                .collect(Collectors.joining("\n")));
+        LexElement parsed = Interpreter.parse(tokenList);
+        System.out.println(calculation + " = " + parsed.eval());
     }
 }
