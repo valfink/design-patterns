894718325f01d54bb77c8cbf873330981809cc72
valfink <117605562+valfink@users.noreply.github.com>
2023-07-11 11:05:32 +0200

    Add TokenType and Token

---
+++ behavioral/interpreter/src/main/java/org/example/Token.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public record Token(
+        TokenType type,
+        String text
+) {
+}
---
+++ behavioral/interpreter/src/main/java/org/example/TokenType.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public enum TokenType {
+    INTEGER,
+    PLUS,
+    MINUS,
+    LEFT_PARENTHESIS,
+    RIGHT_PARENTHESIS
+}
