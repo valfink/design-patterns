c4580134ae17fc1e857aa1a7e9ea6c06f14b716e
valfink <117605562+valfink@users.noreply.github.com>
2023-07-12 17:00:28 +0200

    Use BankAccount & BankAccountMemento

--- behavioral/memento/src/main/java/org/example/Main.java
+++ behavioral/memento/src/main/java/org/example/Main.java
@@ -2,6 +2,20 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BankAccount bankAccount = new BankAccount(100);
+        BankAccountMemento memento1 = bankAccount.deposit(200);
+        BankAccountMemento memento2 = bankAccount.deposit(50);
+
+        System.out.println("Balance currently is: " + bankAccount.getBalance());
+
+        bankAccount.returnToState(memento1);
+        System.out.println("Balance now is: " + bankAccount.getBalance());
+
+        bankAccount.resetToInitialState();
+        System.out.println("Balance now is: " + bankAccount.getBalance());
+
+        bankAccount.returnToState(memento2);
+        System.out.println("Balance now is: " + bankAccount.getBalance());
+        ;
     }
 }
