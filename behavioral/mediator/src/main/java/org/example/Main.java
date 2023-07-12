5ceff5ad3e88586a5e8fb6f092b89b699deff0d4
valfink <117605562+valfink@users.noreply.github.com>
2023-07-12 14:48:53 +0200

    Use Person and ChatRoom

--- behavioral/mediator/src/main/java/org/example/Main.java
+++ behavioral/mediator/src/main/java/org/example/Main.java
@@ -2,6 +2,16 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Person wolfgangAmadeusMozart = new Person("Wolfgang Amadeus Mozart");
+        Person ludwigVanBeethoven = new Person("Ludwig van Beethoven");
+        ChatRoom chatRoom = new ChatRoom();
+        chatRoom.join(wolfgangAmadeusMozart);
+        chatRoom.join(ludwigVanBeethoven);
+        wolfgangAmadeusMozart.send("Hi peeps!");
+        ludwigVanBeethoven.send("Hey Wolfi!");
+        Person johannSebastianBach = new Person("Johann Sebastian Bach");
+        chatRoom.join(johannSebastianBach);
+        johannSebastianBach.send("Hey guys! :)");
+        wolfgangAmadeusMozart.privateMsg("Ludwig van Beethoven", "Oh no, I hoped that he wouldn't show up :(");
     }
 }
