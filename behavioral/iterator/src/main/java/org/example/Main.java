0501ff4291473fe4ec2cf4396d095384e4565886
valfink <117605562+valfink@users.noreply.github.com>
2023-07-12 13:32:25 +0200

    Use PreOrderIterator

--- behavioral/iterator/src/main/java/org/example/Main.java
+++ behavioral/iterator/src/main/java/org/example/Main.java
@@ -1,7 +1,27 @@
 package org.example;
 
+import java.util.Iterator;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Node<String> testNode = new Node<>(
+                "Hello",
+                new Node<>("World"),
+                new Node<>(
+                        "This",
+                        new Node<>("Is"),
+                        new Node<>(
+                                "Major",
+                                new Node<>("Tom"),
+                                new Node<>("!")
+                        )
+                )
+        );
+
+        BinaryTree<String> tree = new BinaryTree<>(testNode);
+        Iterator<String> iterator = tree.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
     }
 }
