168f7c4084302a31d9fc2bad196a439ceadb19ef
valfink <117605562+valfink@users.noreply.github.com>
2023-07-13 09:52:48 +0200

    Use the classes

--- behavioral/observer/src/main/java/org/example/Main.java
+++ behavioral/observer/src/main/java/org/example/Main.java
@@ -2,6 +2,15 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Person person = new Person();
+        Subscription<PropertyChangedEvent<Person>> subscription = person.subScribe(
+                e -> System.out.println("Person's " + e.propertyName() + " has changed: " + e.source())
+        );
+        try (subscription) {
+            person.setAge(30);
+            person.setAge(32);
+        }
+        person.setAge(34);
+        System.out.println("You didn't get notified about the new state: " + person);
     }
 }
